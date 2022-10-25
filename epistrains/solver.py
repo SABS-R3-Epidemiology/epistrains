152ff5dc4aa913b945742afdf8dcfb4f3cfa58cf
Jiayuan Zhu <jiayuanz0322@gmail.com>
2022-10-25 14:32:50 +0100

    flake8

--- epistrains/solver.py
+++ epistrains/solver.py
@@ -3,17 +3,17 @@ import numpy as np
 import scipy.integrate
 from epistrains.strain import Strain
 
+
 class Solver:
-    """
-    :param strains: a list of strains to be infected
+    """:param strains: a list of strains to be infected
     :type strains: _type_
-    :param t_eval: a list of arguments for start time, end time and time interval
+    :param t_eval: a list of arguments for start time,
+        end time and time interval
     :type t_eeval: list
-    :param time: timeframe (h) over which the models should be solved for, 
+    :param time: timeframe (h) over which the models should be solved for,
         defaults to 1
-    :type time: float, optional
-    """
-    def __init__(self, strains: List[Strain], time = 1):
+    :type time: float, optional"""
+    def __init__(self, strains: List[Strain], time=1):
         self.time = round(time, 2)
         self.solution = []
         self.strains = strains
@@ -24,10 +24,11 @@ class Solver:
         t_eval = np.linspace(0, self.time, int(self.time*10000))
         y0 = np.array([0.0, 0.0] + [0.0 for _ in self.strains])
         sol = scipy.integrate.solve_ivp(
-            fun = lambda t, y: self.ODE(t, y, ), #finish by Nathan's fun name
-            t_span = [self.t_eval[0], self.t_eval[-1]],
-            y0 = y0,
-            t_eval = self.t_eval,
-            max_step = 0.001
-        ) 
+            # finish by Nathan's fun name
+            fun=lambda t, y: self.ODE(t, y, ),
+            t_span=[self.t_eval[0], self.t_eval[-1]],
+            y0=y0,
+            t_eval=t_eval,
+            max_step=0.001
+        )
         self.solution = sol
--- epistrains/tests/test_solver.py
+++ epistrains/tests/test_solver.py
@@ -10,11 +10,13 @@ class SolverTest(unittest.TestCase):
         """
         Tests Solver creation.
         """
-        s = es.Solver(strains = [es.Strain(0.1, 0.2, 0.3), es.Strain(0.1, 0.2, 0.3)])
+        s = es.Solver(strains=[es.Strain(0.1, 0.2, 0.3),
+                               es.Strain(0.1, 0.2, 0.3)])
         pass
 
     def test_solver(self):
-        s = es.Solver(strains = [es.Strain(0.1, 0.2, 0.3), es.Strain(0.1, 0.2, 0.3)])
+        s = es.Solver(strains=[es.Strain(0.1, 0.2, 0.3),
+                      es.Strain(0.1, 0.2, 0.3)])
         self.assertEqual(len(s.solution), 0)
         # s.solve()
         # self.assertEqual(len(s.solution), 1)
