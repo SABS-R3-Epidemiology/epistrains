c29af6c3a19d5f3ea4f050c413f7d6b342e78a47
Jiayuan Zhu <jiayuanz0322@gmail.com>
2022-10-25 10:51:54 +0100

    initialisation

--- epistrains/strain.py
+++ epistrains/strain.py
@@ -1,3 +1,5 @@
 class Strain:
-    def __init__(self):
-        pass
+    def __init__(self, die: float, recover: float, transmission: float):
+        self.die = die
+        self.recover = recover
+        self.transmission = transmission
