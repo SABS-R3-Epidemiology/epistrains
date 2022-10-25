71d048b386a30ac4cfa0a6670a64bbf28ee001e5
Jiayuan Zhu <jiayuanz0322@gmail.com>
2022-10-25 10:47:13 +0100

    add solver func

--- epistrains/solver.py
+++ epistrains/solver.py
@@ -1,3 +1,24 @@
+from typing import List
+import numpy as np
+import scipy.integrate
+from epistrains.strain import Strain
+
 class Solver:
-    def __init__(self):
-        pass
+    def __init__(self, strains: List(Strain), time = 1):
+        self.time = round(time, 2)
+        self.solution = []
+        self.strains = strains
+   
+
+    def solver(self):
+
+        t_eval = np.linspace(0, self.time, int(self.time*10000))
+        y0 = np.array([0.0, 0.0, 0.0] + [0.0 for _ in self.strains])
+        sol = scipy.integrate.solve_ivp(
+            fun = lambda t, y: self.ODE(t, y, ), #finish by Nathan's fun name
+            t_span = [t_eval[0], t_eval[-1]],
+            y0 = y0,
+            t_eval = t_eval,
+            max_step = 0.001
+        )
+        self.solution = sol
