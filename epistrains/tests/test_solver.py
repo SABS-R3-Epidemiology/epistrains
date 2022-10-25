3e40514334f27de3949c7c2b722b9a1937631dbd
Jiayuan Zhu <jiayuanz0322@gmail.com>
2022-10-25 11:29:57 +0100

    add strains

--- epistrains/tests/test_solver.py
+++ epistrains/tests/test_solver.py
@@ -10,5 +10,11 @@ class SolverTest(unittest.TestCase):
         """
         Tests Solver creation.
         """
-        s = es.Solver()
+        s = es.Solver(strains = [Strain(), Strain()])
         pass
+
+    def test_solver(self):
+        s = es.Solver(strains = [Strain(), Strain()])
+        self.assertEqual(len(s.solution), 0)
+        s.solve()
+        self.assertEqual(len(s.solution), 1)
