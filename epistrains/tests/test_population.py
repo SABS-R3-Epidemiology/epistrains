72fc73db5389ab7f64f4ddf51f992b3c058f7544
Lara Herriott <58111273+laraherriott@users.noreply.github.com>
2022-10-25 11:04:55 +0100

    Adding tests for population class

--- epistrains/tests/test_population.py
+++ epistrains/tests/test_population.py
@@ -8,7 +8,16 @@ class PopulationTest(unittest.TestCase):
     """
     def test_create(self):
         """
-        Tests Population creation.
+        Tests birth function.
         """
-        p = es.Population(100, 1, 1, 2)
-        pass
+        p = es.Population(1, 2, 3)
+        birth = p.birth_function(0)
+        self.assertEqual(2, birth)
+
+    def test_input_birth(self):
+        """
+        Tests birth function can be specified
+        """
+        p = es.Population(1, birth_function=5)
+        birth = p.birth_function()
+        self.assertEqual(5, birth)
