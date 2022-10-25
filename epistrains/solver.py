0a0f78009b20f52afb85a8c63da87ec858593f70
Jiayuan Zhu <jiayuanz0322@gmail.com>
2022-10-25 11:04:15 +0100

    add docstring

--- epistrains/solver.py
+++ epistrains/solver.py
@@ -4,16 +4,24 @@ import scipy.integrate
 from epistrains.strain import Strain
 
 class Solver:
-    def __init__(self, strains: List(Strain), time = 1):
+    """
+    :param strains: a list of strains to be infected
+    :type strains: _type_
+    :param time: timeframe (h) over which the models should be solved for, 
+        defaults to 1
+    :type time: float, optional
+    """
+    def __init__(self, strains: List[Strain], time = 1):
         self.time = round(time, 2)
         self.solution = []
         self.strains = strains
    
 
     def solver(self):
-
+        """Solve the differential equations
+        """
         t_eval = np.linspace(0, self.time, int(self.time*10000))
-        y0 = np.array([0.0, 0.0, 0.0] + [0.0 for _ in self.strains])
+        y0 = np.array([0.0, 0.0] + [0.0 for _ in self.strains])
         sol = scipy.integrate.solve_ivp(
             fun = lambda t, y: self.ODE(t, y, ), #finish by Nathan's fun name
             t_span = [t_eval[0], t_eval[-1]],
