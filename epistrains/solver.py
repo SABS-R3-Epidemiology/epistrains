6f860b6eb104465eb28a9b6785cd74c0a7149f06
Isaac Ellmen <isaacellmen@gmail.com>
2022-10-25 15:12:05 +0100

    Basic port of ODE implementation (not yet working)

--- epistrains/solver.py
+++ epistrains/solver.py
@@ -14,10 +14,48 @@ class Solver:
         defaults to 1
     :type time: float, optional
     """
-    def __init__(self, strains: List[Strain], time=1):
+    def __init__(self, pop, strains: List[Strain], time=1):
+        self.n = len(strains)
         self.time = round(time, 2)
+        self.pop = pop
         self.strains = strains
         self.solution = None
+        if self.n <= 0:
+            raise ValueError('Number of strains must be positive')
+        self.strains = strains
+        self.alpha = []
+        self.beta = []
+        self.nu = []
+        for strain in self.strains:
+            self.alpha.append(strain.alpha)
+            self.beta.append(strain.beta)
+            self.nu.append(strain.nu)
+
+        self.b = pop.death_rate
+        self.func_birth = pop.birth_rate
+
+    def ODE_S(self, y, b):
+        sum_alpha = sum(strain.alpha for strain in self.strains)
+        dS_dt = self.func_birth(sum(y)) - sum_alpha - b*y[0]
+        return dS_dt
+
+    def ODE_I_j(self, y, b, j):
+        dI_j_dt = y[j]*(self.beta[j-1]*y[0] - (b + self.nu[j-1] + self.alpha[j-1]))
+        return dI_j_dt
+
+    def ODE_R(self, y, b):
+        sum_nu = 0
+        for i in range(1,self.n+1):
+            sum_nu += self.nu[i-1]*y[i]
+        dR_dt = -b * y[-1] + sum_nu
+        return dR_dt
+
+    def rhs(self,y):
+        dy = [self.ODE_S(y, self.b)]
+        for i in range (1,self.n+1):
+            dy.append(self.ODE_I_j(y, self.b, i))
+        dy.append(self.ODE_R(y, self.b))
+        return dy
 
     def solve(self):
         """Solve the differential equations
--- epistrains/tests/test_solver.py
+++ epistrains/tests/test_solver.py
@@ -12,5 +12,24 @@ class SolverTest(unittest.TestCase):
         """
         s1 = es.Strain(0.1, 0.2, 0.3)
         s2 = es.Strain(0.1, 0.2, 0.6)
-        s = es.Solver(strains=[s1, s2])
+        br = es.make_br(2.0, 3.0)
+        p = es.Population(0.5, 0, br)
+        s = es.Solver(strains=[s1, s2], pop=p)
         self.assertEqual(s.solution, None)
+
+    def test_1_strain(self):
+        #Include len(strains) = 1
+        s1 = es.Strain(0.1, 0.2, 0.3)
+        br = es.make_br(2.0, 3.0)
+        p = es.Population(0.5, 0, br)
+        s = es.Solver(strains=[s1], pop=p)
+        s.solve()
+        self.assertEqual(3, len(s.solution))
+
+    #def test_multiple_strain(self):
+    #    s = es.Solver(strains,pop)
+    #    s.solve()
+    #    #Include len(strains) = 3
+    #    self.assertEqual(5, len(s.solution))
+
+
