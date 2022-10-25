b370bdb2739f76b4f5106ec89c662a8475815b67
Jiayuan Zhu <jiayuanz0322@gmail.com>
2022-10-25 11:04:59 +0100

    add docstring

--- epistrains/strain.py
+++ epistrains/strain.py
@@ -1,4 +1,12 @@
 class Strain:
+    '''Represent the jth strain of the disease.
+    :param die: death rate if individual is infected with jth strain
+    :type die: float
+    :param recover: recover rate if individual is infected with jth strain
+    :type recover: float
+    :param transmission: transmission rate of jth strain
+    :type transmission: float
+    '''
     def __init__(self, die: float, recover: float, transmission: float):
         self.die = die
         self.recover = recover
