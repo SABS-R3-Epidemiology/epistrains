a1be74fb1172404f080420734557f4fa2a713f44
Lara Herriott <58111273+laraherriott@users.noreply.github.com>
2022-10-25 10:11:43 +0100

    #1 Population parameters set up

--- epistrains/population.py
+++ epistrains/population.py
@@ -1,3 +1,21 @@
 class Population:
-    def __init__(self):
-        pass
+    """
+    Population based parameters for the construction of birth rate and death rate terms
+    Parameters
+    ----------
+    size: int
+        population size
+    death: float
+        constant death rate
+    a: float
+        birth rate constant
+    k: float
+        birth rate exponent constant
+    """
+
+    def __init__(self, size, death, a, k):
+        """Initialize the class and take general population parameters relating to birth and death rates"""
+        self.size = size
+        self.death_rate = death
+        self.birth_a = a
+        self.birth_k = k
