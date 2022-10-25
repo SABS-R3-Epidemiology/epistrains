569c627a4a595a66a70ea49f4357e367c7e024b0
Lara Herriott <58111273+laraherriott@users.noreply.github.com>
2022-10-25 10:37:38 +0100

    #2 Adding birth function dependent

--- epistrains/population.py
+++ epistrains/population.py
@@ -1,10 +1,10 @@
+import math
+
 class Population:
     """
     Population based parameters for the construction of birth rate and death rate terms
     Parameters
     ----------
-    size: int
-        population size
     death: float
         constant death rate
     a: float
@@ -13,9 +13,18 @@ class Population:
         birth rate exponent constant
     """
 
-    def __init__(self, size, death, a, k):
+    def __init__(self, death, a, k):
         """Initialize the class and take general population parameters relating to birth and death rates"""
-        self.size = size
         self.death_rate = death
         self.birth_a = a
         self.birth_k = k
+
+    def birth_rate(self, size):
+        """Define exponential birth rate function
+        Parameters
+        ----------
+        size: int
+            total population size as calculated in model
+        """
+        rate = self.birth_a * math.exp(-self.birth_k * size)
+        return rate
