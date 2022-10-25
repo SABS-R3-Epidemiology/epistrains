304c6392c812428ebc0f9ae888393db5e1a22408
Jiayuan Zhu <jiayuanz0322@gmail.com>
2022-10-25 11:54:23 +0100

    fix errors and test

--- epistrains/solver.py
+++ epistrains/solver.py
@@ -7,26 +7,28 @@ class Solver:
     """
     :param strains: a list of strains to be infected
     :type strains: _type_
+    :param t_eval: a list of arguments for start time, end time and time interval
+    :type t_eeval: list
     :param time: timeframe (h) over which the models should be solved for, 
         defaults to 1
     :type time: float, optional
     """
-    def __init__(self, strains: List[Strain], time = 1):
+    def __init__(self, strains: List[Strain], t_eval, time = 1):
         self.time = round(time, 2)
         self.solution = []
         self.strains = strains
-   
+        self.t_eval = t_eval
 
     def solver(self):
         """Solve the differential equations
         """
-        t_eval = np.linspace(0, self.time, int(self.time*10000))
+        # t_eval = np.linspace(0, self.time, int(self.time*10000))
         y0 = np.array([0.0, 0.0] + [0.0 for _ in self.strains])
         sol = scipy.integrate.solve_ivp(
             fun = lambda t, y: self.ODE(t, y, ), #finish by Nathan's fun name
-            t_span = [t_eval[0], t_eval[-1]],
+            t_span = [self.t_eval[0], self.t_eval[-1]],
             y0 = y0,
-            t_eval = t_eval,
+            t_eval = self.t_eval,
             max_step = 0.001
-        )
+        ) 
         self.solution = sol
