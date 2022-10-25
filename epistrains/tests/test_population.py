bb43c915fe19b9fd9f96cb1ad889edada25cf815
Lara Herriott <58111273+laraherriott@users.noreply.github.com>
2022-10-25 14:08:45 +0100

    #20 Added tests for type errors and update for new structure

--- epistrains/tests/test_population.py
+++ epistrains/tests/test_population.py
@@ -8,16 +8,49 @@ class PopulationTest(unittest.TestCase):
     """
     def test_create(self):
         """
-        Tests birth function.
+        Tests birth function is created with make_br()
         """
-        p = es.Population(1, 2, 3)
-        birth = p.birth_function(0)
+        br = es.make_br(2.0, 3.0)
+        p = es.Population(0.5, 0, br)
+        birth = p.birth_rate(0)
         self.assertEqual(2, birth)
 
     def test_input_birth(self):
         """
         Tests birth function can be specified
         """
-        p = es.Population(1, birth_function=5)
-        birth = p.birth_function
+        p = es.Population(0.5, 0, birth_function=lambda N: 5)
+        birth = p.birth_rate(0)
         self.assertEqual(5, birth)
+
+    def test_death_error(self):
+        """
+        Tests type error rasied for death rate input
+        """
+        with self.assertRaises(TypeError):
+            es.Population(1, 0)
+
+    def test_size_type(self):
+        """
+        Tests type error raised for initial population size
+        """
+        with self.assertRaises(TypeError):
+            es.Population(0.5, 50.5)
+
+    def test_make_br_error(self):
+        """
+        Tests type error raised for inputs to make_br()
+        """
+        with self.assertRasies(TypeError):
+            es.make_br(2, 3.0)
+        with self.assertRasies(TypeError):
+            es.make_br(2.0, 3)
+
+    def test_br_pop_size(self):
+        """
+        Tests type error raised when updating birth rate
+        """
+        br = es.make_br(2.0, 3.0)
+        p = es.Population(0.5, 0, br)
+        with self.assertRaises(TypeError):
+            p.birth_rate(100.0)
