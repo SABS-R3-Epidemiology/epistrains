a6abe7738eb81f25d9eb7ffc00db36b4417543ab
Lara Herriott <58111273+laraherriott@users.noreply.github.com>
2022-10-28 11:30:54 +0100

    Fix death count and immunity test to be more general

--- epistrains/tests/test_solver.py
+++ epistrains/tests/test_solver.py
@@ -13,7 +13,7 @@ class SolverTest(unittest.TestCase):
     def setUp(self):
         s1 = es.Strain(0.1, 0.2, 0.3, 10)
         s2 = es.Strain(0.1, 0.2, 0.6, 5)
-        s3 = es.Strain(0.1, 0.2, 0.6, 1, delay=10)
+        s3 = es.Strain(0.1, 0.2, 0.6, 1)
         self.strains = [s1, s2, s3]
         br = es.make_br(2.0, 3.0)
         p = es.Population(0.5, 100, br)
@@ -51,7 +51,7 @@ class SolverTest(unittest.TestCase):
         # check the length of the new death array
         self.assertEqual(len(s.deaths), len(s.solution.t))
         # check number deaths smaller than number of infected (at previous time step)
-        assert(all(s.deaths[1:] <= (s.solution.y[1, :] + s.solution.y[2, :] + s.solution.y[3, :])[:-1]))
+        assert (all(s.deaths[1:] <= ((s.solution.y[1, :] + s.solution.y[2, :] + s.solution.y[3, :])[:-1])+0.0001))
 
     def test_make_plot(self):
         s = es.Solver(strains=self.strains, pop=self.p)
@@ -68,7 +68,6 @@ class SolverTest(unittest.TestCase):
                 if i != j:
                     assert (not all(ax.lines[i].get_ydata() == ax.lines[j].get_ydata()))
 
-
     def test_identical_strain(self):
         s = es.Solver(strains=[self.strains[0], self.strains[0]], pop=self.p)
         s.solve()
@@ -80,7 +79,7 @@ class SolverTest(unittest.TestCase):
     def test_takes_immunity(self):
         s = es.Solver(strains=self.strains, pop=self.p2)
         s.solve()
-        assert np.array_equal(s.solution.y[:, 0], [(100-16-50), 10, 5, 1, 50.0])
+        assert (all(s.solution.y[:, 0] == [(100-16-50), 10, 5, 1, 50.0]))
 
     @patch('matplotlib.pylab.show')
     def test_plot(self, show):
